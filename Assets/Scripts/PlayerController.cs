6f8914527b9c6b3ee48a18bb86ff334c445f4385
Miquel Bellet <miquel.bellet@gmail.com>
2020-12-02 23:34:42 +0100

    complete characters

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,88 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float maxVelocityX;
-    public float maxVelocityY;
+    public GameObject pistolObj;
     public GameObject bulletQuadPrefab;
 
-    private Rigidbody rgPlayer;
-    private Animator animatorPlayer;
+    private Animator animPLayer;
 
     void Start()
     {
-        rgPlayer = GetComponent<Rigidbody>();
-        animatorPlayer = GetComponent<Animator>();
+        animPLayer = GetComponent<Animator>();
     }
 
     void Update()
     {
-        CharacterMovement();
         CharacterShoot();
-    }
-
-    private void CharacterMovement()
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                //rgPlayer.velocity += Vector3.forward * 0.2f;
-                animatorPlayer.SetBool("walking", false);
-                animatorPlayer.SetBool("running", true);
-            }
-            else
-            {
-                //rgPlayer.velocity += Vector3.forward * 0.1f;
-                animatorPlayer.SetBool("walking", true);
-                animatorPlayer.SetBool("running", false);
-            }
-        }
-        if (Input.GetKey(KeyCode.S) && rgPlayer.velocity.z > -maxVelocityX)
-        {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                rgPlayer.velocity += Vector3.back * 0.2f;
-            }
-            else
-            {
-                rgPlayer.velocity += Vector3.back * 0.1f;
-            }
-        }
-        if (Input.GetKey(KeyCode.D) && rgPlayer.velocity.x < maxVelocityX)
-        {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                rgPlayer.velocity += Vector3.right * 0.2f;
-            }
-            else
-            {
-                rgPlayer.velocity += Vector3.right * 0.1f;
-            }
-        }
-        if (Input.GetKey(KeyCode.A) && rgPlayer.velocity.x > -maxVelocityX)
-        {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                rgPlayer.velocity += Vector3.left * 0.2f;
-            }
-            else
-            {
-                rgPlayer.velocity += Vector3.left * 0.1f;
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //rgPlayer.velocity += Vector3.up * 8f;
-            animatorPlayer.SetBool("jumping", true);
-        }
-        else
-        {
-            var currentAnim = animatorPlayer.GetCurrentAnimatorClipInfo(0);
-            if (currentAnim[0].clip.name != "jump")
-            {
-                animatorPlayer.SetBool("jumping", false);
-            }
-        }
     }
 
     private void CharacterShoot()
@@ -96,7 +27,7 @@ public class PlayerController : MonoBehaviour
             Debug.DrawRay(transform.position, fwd * 50, Color.green);
             RaycastHit objectHit;
             // Shoot raycast
-            if (Physics.Raycast(transform.position, transform.forward, out objectHit, 50))
+            if (Physics.Raycast(pistolObj.transform.position, transform.forward, out objectHit, 50))
             {
                 GameObject bullet = Instantiate(bulletQuadPrefab,
                             objectHit.point + objectHit.normal * 0.01f,
---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScript : MonoBehaviour
+{
+    public float initLife = 100;
+    public GameObject lifeBar;
+
+    void Start()
+    {
+        SetLifeBar(initLife);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SetLifeBar(float life)
+    {
+        var completLifeWidth = lifeBar.transform.GetChild(0).GetComponent<RectTransform>().rect.width;
+        var currentLifeWidth = completLifeWidth * life / initLife;
+        if (currentLifeWidth <= 0) currentLifeWidth = 0;
+        lifeBar.transform.GetChild(0).GetChild(0).transform.GetComponent<RectTransform>().offsetMin = new Vector2(currentLifeWidth, lifeBar.transform.GetChild(0).GetChild(0).transform.GetComponent<RectTransform>().offsetMin.y);
+    }
+}
